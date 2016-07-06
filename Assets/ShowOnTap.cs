481ec5d3e6b0b8d3820787a32d31bb5750e7e24b
iroq <michaelszewczak@gmail.com>
2016-07-06 19:56:59 +0100

    add movement

---
+++ Assets/ShowOnTap.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowOnTap : MonoBehaviour {
+
+    private int taps;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //GetComponent<TextMesh>().text = "Update tap - " + ++taps;
+        }
+	}
+
+    public void OnTap()
+    {
+        GetComponent<TextMesh>().text = "Event tap - " + ++taps;
+    }
+}
