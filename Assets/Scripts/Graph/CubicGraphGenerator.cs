4f35750ada1b23339d6c27de0e4946a4186489c7
Nahikka <dominiska@gmail.com>
2016-07-08 00:01:17 +0200

    add sphere based graph generation algorithm

--- Assets/Scripts/Graph/CubicGraphGenerator.cs
+++ Assets/Scripts/Graph/CubicGraphGenerator.cs
@@ -32,7 +32,7 @@ public class CubicGraphGenerator : IGraphGenerator
             {
                 for (int k = 0; k < partsCount; k++)
                 {
-                    Vector3 randomVector3 = GraphGeneratorHelper.GetRandomVector3(partSize, randomizationPercentage);
+                    Vector3 randomVector3 = GraphGeneratorHelper.GetRandomCartesianCoordinates(partSize, randomizationPercentage);
 
                     Vector3 node = new Vector3(location.x + i * partSize.x, location.y + j * partSize.y,
                         location.x + k * partSize.z) + randomVector3;
