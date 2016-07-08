97c4e735dc82a60153fd54e2842605c926641921
goffin6 <tomajster@gmail.com>
2016-07-08 15:17:53 +0200

    Ghosts slow down when frightend

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -95,6 +95,7 @@ public abstract class Enemy : MonoBehaviour
         ClideRange = 200;
         ChaseTimer = 10;
         FrightenedTimer = 15;
+        Speed = 25;
         //movingPattern = MovingPattern.Chase;
         player = GameManager.Instance.Player.gameObject;
 
@@ -304,11 +305,13 @@ public abstract class Enemy : MonoBehaviour
     }
     private void ChangeIsFrightened()
     {
+        Speed = 25;
         IsFrightened = false;
     }
     public void FrightenEnemy()
     {
         movingPattern = MovingPattern.Frightened;
+        Speed = 15;
         Invoke("ChangeIsFrightened", FrightenedTimer);
     }
     void OnTriggerEnter(Collider col)
